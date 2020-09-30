6f79a5ea91ee334bd625f9f98d74ee457c210703
Mahdiyeh_Rezaei <mahdiyehrezaei4@gmail.com>
2020-09-30 14:13:00 +0330

    first init

---
+++ src/main/java/com/mapsa/springcloudlinux/controller/HelloController.java
@@ -0,0 +1,16 @@
+package com.mapsa.springcloudlinux.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.PublicKey;
+
+@RestController
+@RequestMapping("/")
+public class HelloController {
+    @GetMapping
+    public String sayHello(){
+        return "Hello World!";
+    }
+}
---
+++ src/test/java/com/mapsa/springcloudlinux/SpringCloudLinuxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mapsa.springcloudlinux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudLinuxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
