ed6175d67230f2135cfebd133e560781d1001a19
Mahdiyeh_Rezaei <mahdiyehrezaei4@gmail.com>
2020-10-01 23:47:07 +0330

    updated.

---
+++ spring-cloud-microservice-car-example/spring-cloud-service-car-delivery/src/main/java/ir/mahdiyehrezaei/springcloudservicecardelivery/SpringCloudServiceCarDeliveryApplication.java
@@ -0,0 +1,14 @@
+package ir.mahdiyehrezaei.springcloudservicecardelivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class SpringCloudServiceCarDeliveryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudServiceCarDeliveryApplication.class, args);
+    }
+
+}
