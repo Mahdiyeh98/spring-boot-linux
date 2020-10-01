1325b5733eccd24e6560676776deed69e80eb64a
Mahdiyeh_Rezaei <mahdiyehrezaei4@gmail.com>
2020-10-01 04:55:19 +0330

    currency service updated.

---
+++ spring-cloud-service-currency/src/test/java/ir/mahdiyehrezaei/springcloudservicecurrency/SpringCloudServiceCurrencyApplicationTests.java
@@ -0,0 +1,13 @@
+package ir.mahdiyehrezaei.springcloudservicecurrency;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudServiceCurrencyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
