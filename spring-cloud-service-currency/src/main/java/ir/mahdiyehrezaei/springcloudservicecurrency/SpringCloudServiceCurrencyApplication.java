a4a7fbbf3d30f23e731d31bec5a796f6ca01a05f
Mahdiyeh_Rezaei <mahdiyehrezaei4@gmail.com>
2020-10-01 04:24:57 +0330

    eureka client service added.

---
+++ spring-cloud-service-currency/src/main/java/ir/mahdiyehrezaei/springcloudservicecurrency/SpringCloudServiceCurrencyApplication.java
@@ -0,0 +1,13 @@
+package ir.mahdiyehrezaei.springcloudservicecurrency;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudServiceCurrencyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudServiceCurrencyApplication.class, args);
+    }
+
+}
